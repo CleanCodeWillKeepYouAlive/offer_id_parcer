03f6d9f2e59ec73a28080cb3fcc67fa00b06004e
SoloLearningman <sg26031990@gmail.com>
2019-12-18 23:31:49 +0200

    Refactoring v3

--- src/main/java/server/BasicHttpServer.java
+++ src/main/java/server/BasicHttpServer.java
@@ -9,29 +9,43 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.URI;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class BasicHttpServer {
     private static Logger logger = LogManager.getLogger(BasicHttpServer.class);
     private static OfferController offerController = new OfferController();
 
-    public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8001), 0);
-        HttpContext context = server.createContext("/offer/");
-        context.setHandler(BasicHttpServer::handleRequest);
-        server.start();
+    public static void main(String[] args) throws Exception {
+        BasicHttpServer.run();
     }
 
-    private static void handleRequest(HttpExchange exchange) throws IOException {
-        URI requestURI = exchange.getRequestURI();
-        int offerId = Integer.parseInt(requestURI.toString().replaceAll(
-                "[^0-9]", ""));
 
-        String response = "Selected offer data: \n "
-                + offerController.selectOffer(offerId);
+    public static void run() throws Exception {
+        final ExecutorService ex = Executors.newSingleThreadExecutor();
+        final HttpServer server = HttpServer.create(new InetSocketAddress(8001), 0);
 
-        exchange.sendResponseHeaders(200, response.getBytes().length);
-        OutputStream os = exchange.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
+        server.createContext("/offer/", (HttpExchange exchange) -> {
+            URI requestURI = exchange.getRequestURI();
+            int offerId = Integer.parseInt(requestURI.toString().replaceAll(
+                    "[^0-9]", ""));
+
+            String response = "Selected offer data: \n "
+                    + offerController.selectOffer(offerId);
+
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+            ex.submit(() -> {
+                server.stop(0);
+                System.out.println("HTTP server stopped");
+            });
+        });
+
+        server.setExecutor(ex);
+        server.start();
+        System.out.println("HTTP server started");
     }
 
     private static void printRequestInfo(HttpExchange exchange) {
